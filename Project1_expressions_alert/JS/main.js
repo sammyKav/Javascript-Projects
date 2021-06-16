ba794920d269232316436269ad90e4916a113f5d
sammyKav <skavkewi@icloud.com>
2021-06-16 16:49:03 -0700

    First Java Assignment

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+
+var A = ' Hello World! ';
+/*document.write(A);*/
+var B = " CRAZY! ";
+var math =3+3;//expression
+var Match_num = math+B;
+window.alert(math);
+var C = "Good afternoon has anyone heard someone say \"coconut\" lately?";
+var D = B+A; //concatenate two string vars
+
+/*document.write(D)*/
+
+var E = "Bird Man", namer ="Frog",hobby ="Benjamin Franklin";//assign color
+document.write(namer);
+
+document.write(E);
